5550d832ce36312327afb24981ba9d00a3fa8d3e
Andrew Telleria and David Remington <Guest@epicodus-88.local>
2016-04-25 10:56:05 -0700

    add API call dependencies and method

--- app/src/main/java/com/epicodus/myrestaurants/RestaurantActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/RestaurantActivity.java
@@ -3,9 +3,19 @@ package com.epicodus.myrestaurants;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import java.io.IOException;
+
+
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
 public class RestaurantActivity extends AppCompatActivity {
+    public static final String TAG = RestaurantActivity.class.getSimpleName();
     private TextView mLocationTextView;
 
     @Override
@@ -17,6 +27,27 @@ public class RestaurantActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
-        mLocationTextView.setText("Here are all the locations near:" + location);
+        getRestaurants(location);
+    }
+
+    private void getRestaurants(String location) {
+        YelpService.findRestaurants(location, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    String jsonData = response.body().string();
+                    if (response.isSuccessful()) {
+                        Log.v(TAG, jsonData);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
