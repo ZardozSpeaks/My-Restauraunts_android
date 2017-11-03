1b96e230c26a3b53adaed99b017e87cf718a862c
David Remington <david.ian.remington@gmail.com>
2017-11-03 13:42:12 -0700

    refactor RestaurantPagerAdapter.java

--- app/src/main/java/com/davidremington/myrestaurants/adapters/RestaurantPagerAdapter.java
+++ app/src/main/java/com/davidremington/myrestaurants/adapters/RestaurantPagerAdapter.java
@@ -10,25 +10,25 @@ import com.davidremington.myrestaurants.ui.RestaurantDetailFragment;
 import java.util.ArrayList;
 
 public class RestaurantPagerAdapter extends FragmentPagerAdapter {
-    private ArrayList<Restaurant> mRestaurants;
+    private ArrayList<Restaurant> restaurants;
 
     public RestaurantPagerAdapter(FragmentManager fm, ArrayList<Restaurant> restaurants) {
         super(fm);
-        mRestaurants = restaurants;
+        this.restaurants = restaurants;
     }
 
     @Override
     public Fragment getItem(int position) {
-        return RestaurantDetailFragment.newInstance(mRestaurants.get(position));
+        return RestaurantDetailFragment.newInstance(restaurants.get(position));
     }
 
     @Override
     public int getCount() {
-        return mRestaurants.size();
+        return restaurants.size();
     }
 
     @Override
     public CharSequence getPageTitle(int position) {
-        return mRestaurants.get(position).getName();
+        return restaurants.get(position).getName();
     }
 }
