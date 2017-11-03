42c0db82c10c1ce612784557ffeb658396845962
David Remington <david.ian.remington@gmail.com>
2017-11-03 15:53:41 -0700

    refactor YelpService.java

--- app/src/main/java/com/davidremington/myrestaurants/Constants.java
+++ app/src/main/java/com/davidremington/myrestaurants/Constants.java
@@ -18,4 +18,18 @@ public class Constants {
     //Intent constants
     public static final String POSITION = "position";
     public static final String RESTAURANTS = "restaurants";
+
+    //JSON constant
+    public static final String BUSINESSES = "businesses";
+    public static final String NAME = "name";
+    public static final String PHONE = "display_phone";
+    public static final String WEBSITE_URL = "url";
+    public static final String RATING = "rating";
+    public static final String IMAGE_URL = "image_url";
+    public static final String LOCATION = "location";
+    public static final String COORDINATE = "coordinate";
+    public static final String LATITUDE = "latitude";
+    public static final String LONGITUDE = "longitude";
+    public static final String ADDRESS = "display_address";
+    public static final String CATEGORIES = "categories";
 }
--- app/src/main/java/com/davidremington/myrestaurants/services/YelpService.java
+++ app/src/main/java/com/davidremington/myrestaurants/services/YelpService.java
@@ -19,8 +19,31 @@ import okhttp3.Request;
 import okhttp3.Response;
 import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer;
 import se.akerfeldt.okhttp.signpost.SigningInterceptor;
+import timber.log.Timber;
 
+import static com.davidremington.myrestaurants.Constants.ADDRESS;
+import static com.davidremington.myrestaurants.Constants.BUSINESSES;
+import static com.davidremington.myrestaurants.Constants.CATEGORIES;
+import static com.davidremington.myrestaurants.Constants.COORDINATE;
+import static com.davidremington.myrestaurants.Constants.IMAGE_URL;
+import static com.davidremington.myrestaurants.Constants.LATITUDE;
+import static com.davidremington.myrestaurants.Constants.LOCATION;
+import static com.davidremington.myrestaurants.Constants.LONGITUDE;
+import static com.davidremington.myrestaurants.Constants.NAME;
+import static com.davidremington.myrestaurants.Constants.PHONE;
+import static com.davidremington.myrestaurants.Constants.RATING;
+import static com.davidremington.myrestaurants.Constants.WEBSITE_URL;
+import static com.davidremington.myrestaurants.Constants.YELP_BASE_URL;
+import static com.davidremington.myrestaurants.Constants.YELP_LOCATION_QUERY_PARAMETER;
+
+/* OkHttp always returns a possible NullPointer in lint outside of the
+ * anonymous onResponse method. It is guaranteed safe however.
+ * https://github.com/square/okhttp/issues/3635
+ * */
+
+@SuppressWarnings("ConstantConditions")
 public class YelpService {
+
     public static void findRestaurants(String location, Callback callback) {
         String CONSUMER_KEY = Constants.YELP_CONSUMER_KEY;
         String CONSUMER_SECRET = Constants.YELP_CONSUMER_SECRET;
@@ -33,59 +56,54 @@ public class YelpService {
                 .addInterceptor(new SigningInterceptor(consumer))
                 .build();
 
-        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.YELP_BASE_URL).newBuilder();
-        urlBuilder.addQueryParameter(Constants.YELP_LOCATION_QUERY_PARAMETER, location);
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(YELP_BASE_URL).newBuilder();
+        urlBuilder.addQueryParameter(YELP_LOCATION_QUERY_PARAMETER, location);
         String url = urlBuilder.build().toString();
 
         Request request= new Request.Builder()
                 .url(url)
                 .build();
-
         Call call = client.newCall(request);
         call.enqueue(callback);
     }
 
     public ArrayList<Restaurant> processResults(Response response) {
         ArrayList<Restaurant> restaurants = new ArrayList<>();
-
+        String jsonData;
         try {
-            String jsonData = response.body().string();
-            if (response.isSuccessful()) {
-                JSONObject yelpJSON = new JSONObject(jsonData);
-                JSONArray businessesJSON = yelpJSON.getJSONArray("businesses");
-                for (int i = 0; i < businessesJSON.length(); i++) {
-                    JSONObject restaurantJSON = businessesJSON.getJSONObject(i);
-                    String name = restaurantJSON.getString("name");
-                    String phone = restaurantJSON.getString("display_phone");
-                    String website = restaurantJSON.getString("url");
-                    double rating = restaurantJSON.getDouble("rating");
-                    String imageUrl = restaurantJSON.getString("image_url");
-                    double latitude = restaurantJSON.getJSONObject("location")
-                            .getJSONObject("coordinate").getDouble("latitude");
-                    double longitude = restaurantJSON.getJSONObject("location")
-                            .getJSONObject("coordinate").getDouble("longitude");
-                    ArrayList<String> address = new ArrayList<>();
-                    JSONArray addressJSON = restaurantJSON.getJSONObject("location")
-                            .getJSONArray("display_address");
-                    for (int y = 0; y < addressJSON.length(); y++) {
-                        address.add(addressJSON.get(y).toString());
-                    }
-
-                    ArrayList<String> categories = new ArrayList<>();
-                    JSONArray categoriesJSON = restaurantJSON.getJSONArray("categories");
-
-                    for (int y = 0; y < categoriesJSON.length(); y++) {
-                        categories.add(categoriesJSON.getJSONArray(y).get(0).toString());
-                    }
-                    Restaurant restaurant = new Restaurant(name, phone, website, rating,
-                            imageUrl, address, latitude, longitude, categories);
-                    restaurants.add(restaurant);
+            jsonData = response.body().string();
+            JSONObject yelpJSON = new JSONObject(jsonData);
+            JSONArray businessesJSON = yelpJSON.getJSONArray(BUSINESSES);
+            for (int i = 0; i < businessesJSON.length(); i++) {
+                JSONObject restaurantJSON = businessesJSON.getJSONObject(i);
+                String name = restaurantJSON.getString(NAME);
+                String phone = restaurantJSON.getString(PHONE);
+                String website = restaurantJSON.getString(WEBSITE_URL);
+                double rating = restaurantJSON.getDouble(RATING);
+                String imageUrl = restaurantJSON.getString(IMAGE_URL);
+                double latitude = restaurantJSON.getJSONObject(LOCATION)
+                        .getJSONObject(COORDINATE).getDouble(LATITUDE);
+                double longitude = restaurantJSON.getJSONObject(LOCATION)
+                        .getJSONObject(COORDINATE).getDouble(LONGITUDE);
+                ArrayList<String> address = new ArrayList<>();
+                JSONArray addressJSON = restaurantJSON.getJSONObject(LOCATION)
+                        .getJSONArray(ADDRESS);
+                for (int y = 0; y < addressJSON.length(); y++) {
+                    address.add(addressJSON.get(y).toString());
                 }
+
+                ArrayList<String> categories = new ArrayList<>();
+                JSONArray categoriesJSON = restaurantJSON.getJSONArray(CATEGORIES);
+
+                for (int y = 0; y < categoriesJSON.length(); y++) {
+                    categories.add(categoriesJSON.getJSONArray(y).get(0).toString());
+                }
+                Restaurant restaurant = new Restaurant(name, phone, website, rating,
+                        imageUrl, address, latitude, longitude, categories);
+                restaurants.add(restaurant);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
+        } catch (IOException | JSONException e) {
+            Timber.e(e.getMessage());
         }
         return restaurants;
     }
