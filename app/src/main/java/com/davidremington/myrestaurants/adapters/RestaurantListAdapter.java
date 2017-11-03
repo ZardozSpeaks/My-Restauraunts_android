78ca4121524d06beb8814878aee694dbb4bd61bb
David Remington <david.ian.remington@gmail.com>
2017-11-03 13:40:21 -0700

    refactor RestaurantListAdapter.java

--- app/src/main/java/com/davidremington/myrestaurants/adapters/RestaurantListAdapter.java
+++ app/src/main/java/com/davidremington/myrestaurants/adapters/RestaurantListAdapter.java
@@ -1,5 +1,5 @@
-
 package com.davidremington.myrestaurants.adapters;
+
 
 import android.content.Context;
 import android.content.Intent;
@@ -19,73 +19,81 @@ import org.parceler.Parcels;
 
 import java.util.ArrayList;
 
-import butterknife.Bind;
+import butterknife.BindView;
 import butterknife.ButterKnife;
+
+import static com.davidremington.myrestaurants.Constants.POSITION;
+import static com.davidremington.myrestaurants.Constants.RESTAURANTS;
 
 public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAdapter.RestaurantViewHolder> {
     private static final int MAX_WIDTH = 200;
     private static final int MAX_HEIGHT = 200;
 
-    private ArrayList<Restaurant> mRestaurants = new ArrayList<>();
-    private Context mContext;
+    private ArrayList<Restaurant> restaurants = new ArrayList<>();
+    private Context context;
 
     public RestaurantListAdapter(Context context, ArrayList<Restaurant> restaurants) {
-        mContext = context;
-        mRestaurants = restaurants;
+        this.context = context;
+        this.restaurants = restaurants;
     }
 
     @Override
     public RestaurantListAdapter.RestaurantViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.restaurant_list_item, parent, false);
-        RestaurantViewHolder viewHolder = new RestaurantViewHolder(view);
-        return viewHolder;
+        return new RestaurantViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(RestaurantListAdapter.RestaurantViewHolder holder, int position) {
-        holder.bindRestaurant(mRestaurants.get(position));
+        holder.bindRestaurant(restaurants.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return mRestaurants.size();
+        return restaurants.size();
     }
 
-    public class RestaurantViewHolder extends RecyclerView.ViewHolder {
-        @Bind(R.id.restaurantImageView) ImageView mRestaurantImageView;
-        @Bind(R.id.restaurantNameTextView) TextView mNameTextView;
-        @Bind(R.id.categoryTextView) TextView mCategoryTextView;
-        @Bind(R.id.ratingTextView) TextView mRatingTextView;
-        private Context mContext;
+    class RestaurantViewHolder extends RecyclerView.ViewHolder {
+       @BindView(R.id.restaurantImageView) ImageView restaurantImageView;
+       @BindView(R.id.restaurantNameTextView) TextView nameTextView;
+       @BindView(R.id.categoryTextView) TextView categoryTextView;
+       @BindView(R.id.ratingTextView) TextView ratingTextView;
 
-        public RestaurantViewHolder(View itemView) {
+       private Context context;
+
+        RestaurantViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
-            mContext = itemView.getContext();
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    int itemPosition = getLayoutPosition();
-                    Intent intent = new Intent(mContext, RestaurantDetailActivity.class);
-                    intent.putExtra("position", itemPosition + "");
-                    intent.putExtra("restaurants", Parcels.wrap(mRestaurants));
-                    mContext.startActivity(intent);
-                }
+            context = itemView.getContext();
+            itemView.setOnClickListener(view -> {
+                int itemPosition = getLayoutPosition();
+                Intent intent = new Intent(context, RestaurantDetailActivity.class);
+                intent.putExtra(POSITION, String.valueOf(itemPosition));
+                intent.putExtra(RESTAURANTS, Parcels.wrap(restaurants));
+                context.startActivity(intent);
             });
         }
 
-        public void bindRestaurant(Restaurant restaurant) {
-            Picasso.with(mContext).load(restaurant.getImageUrl()).into(mRestaurantImageView);
+        void bindRestaurant(Restaurant restaurant) {
+            setRestaurantImage(restaurant);
+            setRestaurantInfo(restaurant);
+        }
 
-            Picasso.with(mContext)
+        private void setRestaurantInfo(Restaurant restaurant) {
+            nameTextView.setText(restaurant.getName());
+            categoryTextView.setText(restaurant.getCategories().get(0));
+            ratingTextView.setText(String.format("%s: %s /5",
+                    context.getString(R.string.list_adapter_rating),
+                    restaurant.getRating() ));
+        }
+
+        private void setRestaurantImage(Restaurant restaurant) {
+            Picasso.with(context).load(restaurant.getImageUrl()).into(restaurantImageView);
+            Picasso.with(context)
                     .load(restaurant.getImageUrl())
                     .resize(MAX_WIDTH, MAX_HEIGHT)
                     .centerCrop()
-                    .into(mRestaurantImageView);
-
-            mNameTextView.setText(restaurant.getName());
-            mCategoryTextView.setText(restaurant.getCategories().get(0));
-            mRatingTextView.setText("Rating: " + restaurant.getRating() + "/5");
+                    .into(restaurantImageView);
         }
     }
 }
